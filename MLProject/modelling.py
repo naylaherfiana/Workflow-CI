c1f782254bfeda5d5d8a850903e38edaa08faa07
naylaherfiana <naylaherfiana@gmail.com>
2026-07-21 22:09:48 +0700

    Fix MLflow start_run dan update dataset Breast Cancer

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,21 +5,28 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", type=str, default="credit_scoring_preprocessing/credit_scoring_preprocessed.csv")
+parser.add_argument("--data_path", type=str, default="breast_cancer_preprocessing/breast_cancer_preprocessed.csv")
 args = parser.parse_args()
 
-mlflow.set_experiment("CI Credit Scoring")
+mlflow.set_experiment("CI Breast Cancer")
 mlflow.autolog()
 
 data = pd.read_csv(args.data_path)
-X = data.drop(columns=["SeriousDlqin2yrs"])
-y = data["SeriousDlqin2yrs"]
+X = data.drop(columns=["target"])
+y = data["target"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-with mlflow.start_run(run_name="ci_random_forest"):
+# MLflow run already started by mlflow run (MLProject command)
+# so we avoid using with mlflow.start_run() unless there is no active run
+if mlflow.active_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
-    print("Training via MLflow Project selesai.")
+else:
+    with mlflow.start_run(run_name="ci_random_forest"):
+        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        model.fit(X_train, y_train)
+
+print("Training via MLflow Project selesai.")
